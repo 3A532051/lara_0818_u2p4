3f2ce522dad6d8f154ab0fb121def3747c2d8ce7
3A532051 <chu123520@gmail.com>
2018-10-31 13:27:48 +0800

    錯誤修正

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,53 +9,54 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Clean Blog</title>
+    <title>@yield('title')</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Theme CSS -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
+    <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
 </head>
 
 <body>
 
+<!-- Navigation -->
+@include('layouts.partials.navigation')
 
-    <!-- Navigation -->
-    @include('layouts.partials.navigation')
-    @yield('comtent')
-
-    <hr>
-
-    <!-- Footer -->
-    @include('layouts.partials.footer')
+@yield('content')
 
 
-    <!-- jQuery -->
-    <script src="{{asset('js/jquery.min.js')}}"></script>
+<hr>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+<!-- Footer -->
+@include('layouts.partials.footer')
 
-    <!-- Contact Form JavaScript -->
-    <script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
-    <script src="{{asset('js/contact_me.js')}}"></script>
 
-    <!-- Theme JavaScript -->
-    <script src="{{asset('js/clean_blog.min.js')}}"></script>
+<!-- jQuery -->
+<script src="{{asset('js/jquery.min.js')}}"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
+<script src="{{asset('js/contact_me.js')}}"></script>
+
+<!-- Theme JavaScript -->
+<script src="{{asset('js/clean-blog.min.js')}}"></script>
 
 </body>
 
